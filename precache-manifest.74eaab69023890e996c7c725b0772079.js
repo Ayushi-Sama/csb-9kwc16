460f6ff2d4be8a52a9834544a91acf3cd936e8b9
Code Sandbox <hello@codesandbox.io>
2022-11-12 13:16:30 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.74eaab69023890e996c7c725b0772079.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f1f75d6dee6bb37b0a070afca85bca",
+    "url": "https://ayushi-sama.github.io/csb-9kwc16/index.html"
+  },
+  {
+    "revision": "4fd7a81c038202ac1436",
+    "url": "https://ayushi-sama.github.io/csb-9kwc16/static/js/2.824d600c.chunk.js"
+  },
+  {
+    "revision": "1089bcacedd7a598676a",
+    "url": "https://ayushi-sama.github.io/csb-9kwc16/static/js/main.ef85a294.chunk.js"
+  },
+  {
+    "revision": "e73216efe796b9f8270e",
+    "url": "https://ayushi-sama.github.io/csb-9kwc16/static/js/runtime-main.167b9368.js"
+  }
+]);
